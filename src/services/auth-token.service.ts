27f31c4d4b696b4b407e1f862a0da92124bd0d4f
Raf-Avetyan <rafikavetyan210907@gmail.com>
2024-08-05 10:17:20 +0400

    Changed cookie set settings

--- src/services/auth-token.service.ts
+++ src/services/auth-token.service.ts
@@ -12,7 +12,7 @@ export const getAccessToken = () => {
 
 export const saveTokenStorage = (accessToken: string) => {
 	Cookies.set(EnumTokens.ACCESS_TOKEN, accessToken, {
-		sameSite: 'Strict',
+		sameSite: 'none',
 		domain: '.vercel.app',
 		expires: 1,
 		secure: true
