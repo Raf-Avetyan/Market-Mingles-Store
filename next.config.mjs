4ab29a0043ffc27e4b1c0b96952aad2f27fb4338
Raf-Avetyan <rafikavetyan210907@gmail.com>
2024-08-06 15:25:43 +0400

    Change images remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'marketmingles.s3.eu-north-1.amazonaws.com'
-			}
-		]
+		domains: ['marketmingles.s3.eu-north-1.amazonaws.com']
 	},
 	experimental: { missingSuspenseWithCSRBailout: false }
 }
