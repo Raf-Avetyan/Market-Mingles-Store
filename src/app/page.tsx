c7fbf6bf62e47842d53feed5e02e7724fc7f990f
Raf-Avetyan <rafikavetyan210907@gmail.com>
2024-08-06 07:58:07 +0400

    Change page.tsx in app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+
+import { DASHBOARD_PAGES } from '@/config/pages.config'
+
 export default function Page() {
+	const { replace } = useRouter()
+
+	useEffect(() => {
+		replace(`https://www.marketmingles.shop${DASHBOARD_PAGES.HOME}`)
+	}, [])
+
 	return <div></div>
 }
