138893f3499207eb2e7fe9200230ce479539901c
Raf-Avetyan <rafikavetyan210907@gmail.com>
2024-08-07 20:49:42 +0400

    Add sidebar default width in small devices

--- src/components/dashboard-layout/DashboardLayout.tsx
+++ src/components/dashboard-layout/DashboardLayout.tsx
@@ -55,7 +55,7 @@ export default function DashboardLayout({ children }: PropsWithChildren) {
 			style={{
 				gridTemplateColumns:
 					sidebarWidth < 1.4
-						? `80px ${mainWidth}fr`
+						? `90px ${mainWidth}fr`
 						: `${sidebarWidth}fr ${mainWidth}fr`
 			}}
 		>
