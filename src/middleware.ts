4cc2198962077dd45b45b68038ce6e2a2c5542f0
Raf-Avetyan <rafikavetyan210907@gmail.com>
2024-08-09 23:38:13 +0400

    Added global loader

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,7 +11,7 @@ export async function middleware(request: NextRequest, response: NextResponse) {
 	const isAuthPage = url.includes('/auth')
 
 	if (isAuthPage && refreshToken) {
-		return NextResponse.redirect(new URL(DASHBOARD_PAGES.HOME, url))
+		return NextResponse.redirect(new URL('/', url))
 	}
 
 	if (isAuthPage) {
